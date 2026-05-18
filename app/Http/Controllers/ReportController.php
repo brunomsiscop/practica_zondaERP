80d5c2d4a1a940a667400afba6b88e856e3952b7
diego-dcr <omenplus.ddrc@gmail.com>
2026-05-18 10:25:57 -0600

    Zonda ERP - Revision de reportes (product) v1.1

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1254,8 +1254,11 @@ class ReportController extends Controller
 
     public function destroyProduct(string $dataId)
     {
-
+        dd($dataId);
+        dd('...');
         $order_product = OrderProduct::find($dataId);
+
+        dd($order_product);
         $order = Order::find($order_product->order_id);
 
         $order_product->delete();
